98ceb869e429430332d049d4f7d829677c39f533
DrvnkMario <mariandoroszo@gmial.com>
2022-05-15 13:55:46 +0200

    Change user control apperance and add it's properties and events.

--- MVVM_MiniTotalCommander/View/PanelTC.xaml.cs
+++ MVVM_MiniTotalCommander/View/PanelTC.xaml.cs
@@ -12,11 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace MVVM_MiniTotalCommander.View
 {
     /// <summary>
-    /// Logika interakcji dla klasy PanelTC.xaml
+    /// Logika interakcji dla klasy UserControl1.xaml
     /// </summary>
     public partial class PanelTC : UserControl
     {
@@ -24,5 +23,91 @@ namespace MVVM_MiniTotalCommander.View
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                    nameof(currPath),
+                    typeof(string),
+                    typeof(PanelTC)
+                );
+        public string currPath
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ComboBoxContentProperty =
+            DependencyProperty.Register(
+                    nameof(comboContent),
+                    typeof(List<String>),
+                    typeof(PanelTC)
+                );
+        public List<String> comboContent
+        {
+            get { return (List<String>)GetValue(ComboBoxContentProperty); }
+            set { SetValue(ComboBoxContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty ListBoxContentProperty =
+            DependencyProperty.Register(
+                    nameof(listBoxContent),
+                    typeof(List<String>),
+                    typeof(PanelTC)
+                );
+        public List<String> listBoxContent
+        {
+            get { return (List<String>)GetValue(ListBoxContentProperty); }
+            set { SetValue(ListBoxContentProperty, value); }
+        }
+
+        public static readonly RoutedEvent evComboBox_ContextMenuOpening =
+        EventManager.RegisterRoutedEvent(nameof(evComboBox_ContextMenuOpening_handler),
+                   RoutingStrategy.Bubble, typeof(RoutedEventHandler),
+                   typeof(PanelTC));
+
+
+        public event RoutedEventHandler evComboBox_ContextMenuOpening_handler
+        {
+            add { AddHandler(evComboBox_ContextMenuOpening, value); }
+            remove { RemoveHandler(evComboBox_ContextMenuOpening, value); }
+        }
+
+        void raise_ComboBox_ContextMenuOpening()
+        {
+            RoutedEventArgs newEventArgs =
+                    new RoutedEventArgs(PanelTC.evComboBox_ContextMenuOpening);
+            RaiseEvent(newEventArgs);
+        }
+
+        private void ComboBox_ContextMenuOpening(object sender, EventArgs e)
+        {
+            Console.WriteLine("CC: " + comboContent.Count);
+            raise_ComboBox_ContextMenuOpening();
+        }
+
+
+        public static readonly RoutedEvent ListBox_doubleClick =
+       EventManager.RegisterRoutedEvent(nameof(ListBox_doubleClick_handler),
+                    RoutingStrategy.Bubble, typeof(RoutedEventHandler),
+                    typeof(PanelTC));
+
+        public event RoutedEventHandler ListBox_doubleClick_handler
+        {
+            add { AddHandler(ListBox_doubleClick, value); }
+            remove { RemoveHandler(ListBox_doubleClick, value); }
+        }
+
+        void raiseListBox_doubleClick()
+        {
+            RoutedEventArgs newEventArgs =
+                    new RoutedEventArgs(PanelTC.ListBox_doubleClick);
+            RaiseEvent(newEventArgs);
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            raiseListBox_doubleClick();
+        }
+
     }
 }
