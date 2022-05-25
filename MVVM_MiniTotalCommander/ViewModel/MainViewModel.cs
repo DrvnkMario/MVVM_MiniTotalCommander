d32b467e3222fd6c5c9ebd0532f595210f076500
DrvnkMario <mariandoroszo@gmial.com>
2022-05-25 11:12:51 +0200

    Finished project

--- MVVM_MiniTotalCommander/ViewModel/MainViewModel.cs
+++ MVVM_MiniTotalCommander/ViewModel/MainViewModel.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Windows.Input;
-using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Collections.ObjectModel;
 namespace MVVM_MiniTotalCommander.ViewModel
 {
     public class MainViewModel
     {
         public TCPanelViewModel LeftPanel { get; set; }
         public TCPanelViewModel RightPanel { get; set; }
-
         public MainViewModel()
         {
             LeftPanel = new TCPanelViewModel();
@@ -19,7 +20,41 @@ namespace MVVM_MiniTotalCommander.ViewModel
         public ICommand Copy => copy ?? (copy =
             new RelayCommand(o =>
             {
-                Console.WriteLine(LeftPanel.CurrentPath);
+                try
+                {
+                    if (LeftPanel.IsSelected == true)
+                    {
+                        Console.WriteLine(LeftPanel.CurrentPath + LeftPanel.ListBoxSelectedItem);
+                        Console.WriteLine(RightPanel.CurrentPath + LeftPanel.ListBoxSelectedItem);
+                        File.Copy(LeftPanel.CurrentPath + LeftPanel.ListBoxSelectedItem, RightPanel.CurrentPath + LeftPanel.ListBoxSelectedItem);
+                    }
+                    else if (RightPanel.IsSelected == true)
+                    {
+                        File.Copy(RightPanel.CurrentPath + RightPanel.ListBoxSelectedItem, LeftPanel.CurrentPath +  RightPanel.ListBoxSelectedItem);
+                    }
+                    // Reloading left and right panel content
+                    LeftPanel.panelTC.getDirectoriesAndFiles(LeftPanel.CurrentPath);
+                    LeftPanel.DirsAndFiles = new ObservableCollection<string>(LeftPanel.panelTC.SubDirsAndFiles);
+                    RightPanel.panelTC.getDirectoriesAndFiles(RightPanel.CurrentPath);
+                    RightPanel.DirsAndFiles = new ObservableCollection<string>(RightPanel.panelTC.SubDirsAndFiles);
+
+                }
+                catch (System.UnauthorizedAccessException)
+                {
+                    MessageBox.Show("You can not access this path.", "Error", MessageBoxButton.OK);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    MessageBox.Show("Could not find target path.", "Error", MessageBoxButton.OK);
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("Wrong file.", "Error", MessageBoxButton.OK);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("The file already exists.", "Error", MessageBoxButton.OK);
+                }
             }, null));
     }
 }
---
+++ MVVM_MiniTotalCommander/ViewModel/TCPanelViewModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+namespace MVVM_MiniTotalCommander.ViewModel
+{
+    public class TCPanelViewModel : INotifyPropertyChanged
+    {
+        #region properties
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Model.PanelTC panelTC { get; set; }
+        private ObservableCollection<string> availableDirectories { get; set; }
+        public ObservableCollection<string> AvailableDirectories
+        {
+            get => availableDirectories;
+            set
+            {
+                availableDirectories = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AvailableDirectories)));
+            }
+        }
+        private ObservableCollection<string> dirsAndFiles { get; set; }
+        public ObservableCollection<string> DirsAndFiles
+        {
+            get => dirsAndFiles;
+            set
+            {
+                dirsAndFiles = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DirsAndFiles)));
+            }
+        }
+        private string currentPath { get; set; }
+        public string CurrentPath
+        {
+            get => currentPath;
+            set
+            {
+                currentPath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentPath)));
+            }
+        }
+        private string listBoxSelectedItem { get; set; }
+        public string ListBoxSelectedItem
+        {
+            get =>listBoxSelectedItem;
+            set
+            {
+                listBoxSelectedItem = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ListBoxSelectedItem)));
+            }
+        }
+        private bool isSelected { get; set; }
+        public bool IsSelected
+        {
+            get => isSelected;
+            set
+            {
+                isSelected = value;
+            }
+        }
+        #endregion
+        #region constructors and initialization of properties
+        public TCPanelViewModel()
+        {
+            panelTC = new Model.PanelTC();
+            panelTC.searchForAvailableDrives();
+            AvailableDirectories = panelTC.AvailableDirectories;
+            CurrentPath = AvailableDirectories[0];
+            panelTC.getDirectoriesAndFiles(CurrentPath);
+            DirsAndFiles = panelTC.SubDirsAndFiles;
+            isSelected = false;
+        }
+        #endregion
+        #region ICommand event handling
+
+        // Handling ListBox item double click with ICommand
+        // This method changes path and refreshes ListBox content
+        private ICommand listBox_db_click { get; set; }
+        public ICommand ListBox_db_click => listBox_db_click ?? (listBox_db_click = new RelayCommand(o =>
+        {
+            if (ListBoxSelectedItem != null)
+            {
+                if (ListBoxSelectedItem.StartsWith("<D>") == true)
+                {
+                    if (Path.GetPathRoot(CurrentPath) == CurrentPath)
+                    {
+                        this.CurrentPath += ListBoxSelectedItem.Remove(0, 3) + "\\";
+                    }
+                    else
+                    {
+                        this.CurrentPath += ListBoxSelectedItem.Remove(0, 3) + "\\";
+                    }
+                }
+                else if (ListBoxSelectedItem == "..")
+                {
+                    CurrentPath = CurrentPath.Remove(CurrentPath.Length - 1);
+                    CurrentPath = Path.GetDirectoryName(CurrentPath);
+                }
+            }
+            panelTC.getDirectoriesAndFiles(CurrentPath);
+            DirsAndFiles = new ObservableCollection<string>(panelTC.SubDirsAndFiles);
+        }, null));
+
+
+        // Handling ComboBox Dropdown opening with ICommand
+        // This method refreshes content of ComboBox
+        private ICommand comboBox_ContentMenuOpening { get; set; }
+        public ICommand ComboBox_ContentMenuOpening => comboBox_ContentMenuOpening ?? (comboBox_ContentMenuOpening =
+            new RelayCommand(o =>
+            {
+                panelTC.searchForAvailableDrives();
+                availableDirectories = new ObservableCollection<string>(panelTC.AvailableDirectories);
+            }, null));
+
+        // Handling Combobox Dropdown close with ICommand
+        // This method updates ListBox content
+        private ICommand comboBox_DropdownClose { get; set; }
+        public ICommand ComboBox_DropdownClose => comboBox_DropdownClose ?? (comboBox_DropdownClose =
+            new RelayCommand( o=>
+            {
+                panelTC.getDirectoriesAndFiles(CurrentPath);
+                DirsAndFiles = new ObservableCollection<string>(panelTC.SubDirsAndFiles);
+
+            }, null));
+
+        private ICommand gotFocus { get; set; }
+        public ICommand GotFocus => gotFocus ?? (gotFocus =
+            new RelayCommand(o =>
+           {
+               isSelected = true;
+           }, null));
+
+        private ICommand lostFocus { get; set; }
+        public ICommand LostFocus => lostFocus ?? (lostFocus =
+            new RelayCommand(o =>
+           {
+               isSelected = false;
+           }, null));
+        #endregion
+    }
+}
